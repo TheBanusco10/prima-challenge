86cfc62a9abcec05d6d3272e28d17df9b8477fe4
David Jimenez <thebanusco10@gmail.com>
2026-03-07 14:13:50 +0100

    Add CoreCard component

---
+++ src/features/core/components/CoreCard.tsx
@@ -0,0 +1,27 @@
+interface Props {
+  Title?: () => React.ReactNode;
+  Content?: () => React.ReactNode;
+  Actions?: () => React.ReactNode;
+  Image?: () => React.ReactNode;
+  OverImage?: () => React.ReactNode;
+}
+
+function CoreCard({ Title, Content, Actions, Image, OverImage }: Props) {
+  return (
+    <div className="card bg-base-100 w-96 shadow-sm">
+      {Image && (
+        <figure className="relative">
+          <Image />
+          {OverImage && <OverImage />}
+        </figure>
+      )}
+      <div className="card-body">
+        {Title && <h2 className="card-title">{Title()}</h2>}
+        {Content && <p>{Content()}</p>}
+        {Actions && <div className="card-actions">{Actions()}</div>}
+      </div>
+    </div>
+  );
+}
+
+export default CoreCard;
